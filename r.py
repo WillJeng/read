bcc1df19347846b2cf914709d0e315ea6a9f0281
Will Jeng <vitginboy3@yahoo.com.tw>
2022-05-21 16:59:16 +0800

    add basic version2

--- r.py
+++ r.py
@@ -8,5 +8,11 @@ with open("reviews.txt", "r") as f:
  		if count % 1000 == 0:
  			print(len(data))
 
-print(len(data))
+print("檔案讀取完成,總共有", len(data), "筆資料")
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print("總長度為:", sum_len, "個字")
+print("留言的平均長度", sum_len/len(data))
 #print(data[0])
